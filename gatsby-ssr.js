e9507fd24e38228fb0b6c4002736ecf68ca9d64e
Juan Cruz Lafranconi <jlafranconi@uade.edu.ar>
2022-07-29 15:28:18 -0300

    add themes

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,7 +3,18 @@
  *
  * See: https://www.gatsbyjs.com/docs/ssr-apis/
  */
+ import React from "react";
+ import { ThemeProvider } from "styled-components";
+ import Theme from "./src/themes/theme";
+ 
+ export const wrapRootElement = ({ element }) => (
+     //this wraps the entire aplicaton with the themes
+     <ThemeProvider theme={Theme}>
+         {element} //react aplication
+     </ThemeProvider>
+ )
 
 exports.onRenderBody = ({ setHtmlAttributes }) => {
   setHtmlAttributes({ lang: `en` })
 }
+
