a9eea8d985cb20668b304c3d3eac5793e1eb3437
Juan Cruz Lafranconi <jlafranconi@uade.edu.ar>
2022-07-29 14:34:42 -0300

    works

---
+++ gatsby-config.js
@@ -0,0 +1,88 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+  },
+  plugins: [
+    `gatsby-plugin-image`,
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name:`pages`,
+        path:`${__dirname}/src/pages`
+      },
+    },
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name:`posts`,
+        path:`${__dirname}/src/posts`
+      },
+    },
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    // Changes images quality
+
+    {
+      resolve: `gatsby-plugin-mdx`,
+      // markdown
+      options: {
+        extends: [`.md`,`.mdx`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options:{
+              maxWidth: 1200,
+              //max width of images
+            },
+          },
+        ],
+      },
+    },
+
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `limelight`,
+          `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
+        ],
+        display: 'swap'
+        // it uses fallback font until the font loads
+      }
+    },
+    
+
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
